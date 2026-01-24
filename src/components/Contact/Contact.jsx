e756241bdf6d53086822d970d7d0541e4e678eb1
Ashik <nafisfoisal25@gmail.com>
2026-01-24 23:49:04 +0600

    Initial commit

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,108 @@
+import React, { useRef } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(form.current);
+    const name = formData.get("user_name");
+    const email = formData.get("user_email");
+    const subject = formData.get("subject");
+    const message = formData.get("message");
+
+    const mailtoLink = `mailto:nafisfoisal25@gmail.com
+      ?subject=${encodeURIComponent(subject)}
+      &body=${encodeURIComponent(
+        `Name: ${name}\nEmail: ${email}\n\nMessage:\n${message}`
+      )}`;
+
+    // Open email client
+    window.location.href = mailtoLink;
+
+    // Optional success message
+    toast.success("Email client opened successfully ", {
+      position: "top-right",
+      autoClose: 3000,
+      theme: "dark",
+    });
+
+    // Reset form
+    form.current.reset();
+  };
+
+  return (
+    <section
+      id="contact"
+      className="flex flex-col items-center justify-center py-24 px-[12vw] md:px-[7vw] lg:px-[20vw]"
+    >
+      <ToastContainer />
+
+      {/* Title */}
+      <div className="text-center mb-16">
+        <h2 className="text-4xl font-bold text-white">CONTACT</h2>
+        <div className="w-32 h-1 bg-purple-500 mx-auto mt-4"></div>
+        <p className="text-gray-400 mt-4 text-lg font-semibold">
+          I’d love to hear from you—reach out anytime!
+        </p>
+      </div>
+
+      {/* Form */}
+      <div className="mt-8 w-full max-w-md bg-[#0d081f] p-6 rounded-lg shadow-lg border border-gray-700">
+        <h3 className="text-xl font-semibold text-white text-center">
+          Connect With Me 
+        </h3>
+
+        <form
+          ref={form}
+          onSubmit={sendEmail}
+          className="mt-4 flex flex-col space-y-4"
+        >
+          <input
+            type="email"
+            name="user_email"
+            placeholder="Your Email"
+            required
+            className="w-full p-3 rounded-md bg-[#131025] text-white border border-gray-600 focus:outline-none focus:border-purple-500"
+          />
+
+          <input
+            type="text"
+            name="user_name"
+            placeholder="Your Name"
+            required
+            className="w-full p-3 rounded-md bg-[#131025] text-white border border-gray-600 focus:outline-none focus:border-purple-500"
+          />
+
+          <input
+            type="text"
+            name="subject"
+            placeholder="Subject"
+            required
+            className="w-full p-3 rounded-md bg-[#131025] text-white border border-gray-600 focus:outline-none focus:border-purple-500"
+          />
+
+          <textarea
+            name="message"
+            placeholder="Message"
+            rows="4"
+            required
+            className="w-full p-3 rounded-md bg-[#131025] text-white border border-gray-600 focus:outline-none focus:border-purple-500"
+          />
+
+          <button
+            type="submit"
+            className="w-full bg-gradient-to-r from-purple-600 to-pink-500 py-3 text-white font-semibold rounded-md hover:opacity-90 transition"
+          >
+            Send
+          </button>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
---
+++ src/constants.js
@@ -0,0 +1,285 @@
+// Skills Section Logo's
+import htmlLogo from './assets/tech_logo/html.png';
+import cssLogo from './assets/tech_logo/css.png';
+import sassLogo from './assets/tech_logo/sass.png';
+import javascriptLogo from './assets/tech_logo/javascript.png';
+import reactjsLogo from './assets/tech_logo/reactjs.png';
+import angularLogo from './assets/tech_logo/angular.png';
+import reduxLogo from './assets/tech_logo/redux.png';
+import nextjsLogo from './assets/tech_logo/nextjs.png';
+import tailwindcssLogo from './assets/tech_logo/tailwindcss.png';
+import gsapLogo from './assets/tech_logo/gsap.png';
+import materialuiLogo from './assets/tech_logo/materialui.png';
+import bootstrapLogo from './assets/tech_logo/bootstrap.png';
+import springbootLogo from './assets/tech_logo/springboot.png';
+import nodejsLogo from './assets/tech_logo/nodejs.png';
+import expressjsLogo from './assets/tech_logo/express.png';
+import mysqlLogo from './assets/tech_logo/mysql.png';
+import mongodbLogo from './assets/tech_logo/mongodb.png';
+import firebaseLogo from './assets/tech_logo/firebase.png';
+import cLogo from './assets/tech_logo/c.png';
+import cppLogo from './assets/tech_logo/cpp.png';
+import javaLogo from './assets/tech_logo/java.png';
+import pythonLogo from './assets/tech_logo/python.png';
+import typescriptLogo from './assets/tech_logo/typescript.png';
+import gitLogo from './assets/tech_logo/git.png';
+import githubLogo from './assets/tech_logo/github.png';
+import vscodeLogo from './assets/tech_logo/vscode.png';
+import postmanLogo from './assets/tech_logo/postman.png';
+import mcLogo from './assets/tech_logo/mc.png';
+import figmaLogo from './assets/tech_logo/figma.png';
+import netlifyLogo from './assets/tech_logo/netlify.png';
+import vercelLogo from './assets/tech_logo/vercel.png';
+import postgreLogo from './assets/tech_logo/postgre.png';
+import csharpLogo from './assets/tech_logo/csharp.png';
+
+// Experience Section Logo's
+import webverseLogo from './assets/company_logo/webverse_logo.png';
+import codealpha from './assets/company_logo/codealpha.jpeg';
+import agcLogo from './assets/company_logo/agc_logo.png';
+import newtonschoolLogo from './assets/company_logo/newtonschool_logo.png';
+
+// Education Section Logo's
+import glaLogo from './assets/education_logo/gla_logo.png';
+import bsaLogo from './assets/education_logo/bsa_logo.png';
+import vpsLogo from './assets/education_logo/vps_logo.png';
+import kuet from './assets/education_logo/kuet.png';
+import cgc from './assets/education_logo/cgc.png';
+import vj from './assets/education_logo/vj.png';
+
+//Project
+import job from './assets/project/ai_job_finder.png';
+import blood from './assets/project/bloodbank.png';
+import bongochat from './assets/project/bongochat.png';
+import bongomeet from './assets/project/bongomeet.png';
+import bongoshop from './assets/project/bongoshop.png';
+import event from './assets/project/event.png';
+import vehicle from './assets/project/vehicle.png';
+
+
+// Project Section Logo's
+import githubdetLogo from './assets/work_logo/github_det.png';
+import csprepLogo from './assets/work_logo/cs_prep.png';
+import movierecLogo from './assets/work_logo/movie_rec.png';
+import taskremLogo from './assets/work_logo/task_rem.png';
+import npmLogo from './assets/work_logo/npm.png';
+import webverLogo from './assets/work_logo/web_dig.png';
+import cmLogo from './assets/work_logo/cm.png';
+import imagesearchLogo from './assets/work_logo/image_search.png';
+import removebgLogo from './assets/work_logo/remove_bg.png';
+
+
+export const SkillsInfo = [
+  {
+    title: 'Frontend',
+    skills: [
+      { name: 'HTML', logo: htmlLogo },
+      { name: 'CSS', logo: cssLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'React JS', logo: reactjsLogo },
+      { name: 'Redux', logo: reduxLogo },
+      { name: 'Next JS', logo: nextjsLogo },
+      { name: 'Tailwind CSS', logo: tailwindcssLogo },
+      { name: 'Material UI', logo: materialuiLogo },
+      { name: 'Bootstrap', logo: bootstrapLogo },
+    ],
+  },
+  {
+    title: 'Backend',
+    skills: [
+      { name: 'Node JS', logo: nodejsLogo },
+      { name: 'Express JS', logo: expressjsLogo },
+      { name: 'MySQL', logo: mysqlLogo },
+      { name: 'MongoDB', logo: mongodbLogo },
+      { name: 'Firebase', logo: firebaseLogo },
+    ],
+  },
+  {
+    title: 'Languages',
+    skills: [
+      { name: 'C', logo: cLogo },
+      { name: 'C++', logo: cppLogo },
+      { name: 'Java', logo: javaLogo },
+      { name: 'Python', logo: pythonLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'TypeScript', logo: typescriptLogo },
+    ],
+  },
+  {
+    title: 'Tools',
+    skills: [
+      { name: 'Git', logo: gitLogo },
+      { name: 'GitHub', logo: githubLogo },
+      { name: 'VS Code', logo: vscodeLogo },
+      { name: 'Postman', logo: postmanLogo },
+      { name: 'Vercel', logo: vercelLogo },
+      { name: 'Figma', logo: figmaLogo },
+    ],
+  },
+];
+
+  export const experiences = [
+    {
+      id: 0,
+      img: codealpha,
+      role: "Mobile Application Developer",
+      company: "CodeAlpha",
+      date: "Janauary 2025 - March 2025",
+      desc: "Developed dynamic and scalable web applications using the MERN stack, handling both frontend and backend development. Collaborated with cross-functional teams to build responsive UI, implement RESTful APIs, and optimize application performance in an agile environment.",
+      skills: [
+        "Andriod Studio",
+        "JavaScript",
+        "XML",
+        "Firebase",
+        "REST API",
+        "Logcat",
+        "Build & Play Store",
+      ],
+    },
+    /*{
+      id: 1,
+      img: agcLogo,
+      role: "Fullstack Engineer",
+      company: "Agumentik Group of Companies",
+      date: "July 2023 - March 2024",
+      desc: "Contributed to innovative projects as a Fullstack Engineer, leading both frontend and backend development using technologies such as HTML, CSS, JavaScript, PHP, SQL, Bootstrap, and ReactJS. Worked closely with the team to deliver responsive, high-performance web applications and improve user experience through seamless integration of various technologies.",
+      skills: [
+        "ReactJS",
+        "Redux",
+        "JavaScript",
+        "Tailwind CSS",
+        "HTML",
+        "CSS",
+        "SQL",
+      ],
+    },
+    {
+      id: 2,
+      img: newtonschoolLogo,
+      role: "Frontend Intern",
+      company: "Newton School",
+      date: "September 2021 - August 2022",
+      desc: "Worked as a Frontend Developer Intern, designing and implementing scalable UI components and responsive websites using HTML, CSS, JavaScript, Bootstrap, and Material UI. Collaborated with the design team to translate wireframes and prototypes from Figma into interactive, user-friendly web pages.",
+      skills: [
+        "HTML",
+        "CSS",
+        "Javascript",
+        "Bootstrap",
+        "Figma",
+        "Material UI",
+      ],
+    },*/
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: kuet,
+      school: "Khulna University of Engineering & Technology (KUET)",
+      date: "March 2021 - June 2026(Expected)",
+      grade: "3.32 CGPA till 6th semester",
+      desc: "A leading public engineering university in Bangladesh, known for strong programs in engineering, science, and technology, with a focus on research, innovation, and industry-relevant education.",
+      degree: "Bachelor of Computer Science and Engineering (CSE)",
+    },
+    {
+      id: 1,
+      img: cgc,
+      school: "Chuadanga Govt. College, Chuadanga",
+      date: "2018 - 2020",
+      grade: "5.00 GPA",
+      desc: "A government college in Chuadanga offering higher secondary and undergraduate education under the National Curriculum, known for its academic discipline and strong foundational programs.I completed my HSC in Science group from here. Throughout my studies, I was immersed in a variety of subjects that deepened my understanding of computing and technology. ",
+      degree: "Bachelor of Science - BSC (Computer Science)",
+    },
+    {
+      id: 2,
+      img: vj,
+      school: "Victoria Jubilee High School Chuadanga",
+      date: "2015 - 2017",
+      grade: "5.00 GPA",
+      desc: "A well-known secondary school in Chuadanga that provides foundational education with an emphasis on academic excellence and student development. I completed my SSC in Science group from here. ",
+      degree: "Secondary School Certificate (SSC)",
+    },
+    {
+      id: 3,
+      img: vj,
+      school: "Victoria Jubilee High School Chuadanga",
+      date: "2013 - 2015",
+      grade: "5.00 GPA",
+      desc: "A well-known secondary school in Chuadanga that provides foundational education with an emphasis on academic excellence and student development. I completed my JSC in Science group from here. ",
+      degree: "Junior School Certificate (JSC)",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 0,
+      title: "Event Management System",
+      description:
+        "Developed a Laravel-based event and course management system where users can view club details, apply to events, purchase courses, access learning materials, and interact with an AI assistant. The system is built using Laravel with MySQL as the backend, integrated with SSLCommerz for secure payments and Gemini API for AI-powered responses, while the frontend uses HTML, Tailwind CSS, and JavaScript. Admins can create and manage courses, monitor enrolled members, and control access to content. The platform eliminates geographical barriers by enabling students across Bangladesh to access university club workshops and webinars online. Additionally, it reduces paper-based advertisements, contributing to a more eco-friendly digital ecosystem.",
+      image: event,
+      tags: ["HTML", "CSS", "JavaScript", "React JS", "API" , "Node.js", "Express", "Laravel" , "MySQL"],
+      github: "https://github.com/Nafis-Foysal-Ashik/Event_Management.git",
+      //webapp: "https://githubprofiledetective.netlify.app/",
+    },
+    {
+      id: 1,
+      title: "BongoShop",
+      description:
+        "Developed a full-featured e-commerce web application using the MERN technology stack, allowing users to browse product details and securely purchase items after authentication. The frontend was built with React, HTML, Bootstrap, and JavaScript, while MongoDB was used for data storage. Middleware was implemented to ensure secure routing, and multiple payment options including Razorpay, Stripe, and Cash on Delivery were integrated. Toast notifications were added to enhance user feedback and interaction. The system delivers a real-world online shopping experience with real-time price calculation and efficient product and inventory management for both customers and administrators.",
+      image: bongoshop,
+      tags: ["React JS", "Node.js", "MongoDB", "Express", "HTML", "CSS", "JavaScript" , "Bootstrap" ,],
+      github: "https://github.com/Nafis-Foysal-Ashik/E-commerce.git",
+      //webapp: "https://csprep.netlify.app/",
+    },
+    {
+      id: 2,
+      title: "BongoMeet",
+      description:
+        "Designed and developed a web-based online meeting platform similar to Google Meet, enabling users to register, log in, create meetings, and join sessions via shared links. The frontend was built using React.js and Tailwind CSS, while authentication and backend services were implemented with Node.js, Express.js, MongoDB, and localStorage. Real-time features such as participant updates and live communication were enabled using Socket.IO and WebRTC. The platform delivers a smooth and reliable real-time user experience for online meetings. This project demonstrates strong skills in full-stack development and real-time system implementation.",
+      image: bongomeet,
+      tags: ["React JS", "API", "HTML", "CSS", "JavaScript" , "Node.js", "Express", "MongoDB", "Socket.IO", "WebRTC"],
+      github: "https://github.com/Nafis-Foysal-Ashik/Bongo_Meet_Backend.git",
+      //webapp: "https://movie-recommendation-app-jet.vercel.app/",
+    },
+    {
+      id: 3,
+      title: "BongoChat ",
+      description:
+        "Developed a full-stack real-time chat application featuring secure user authentication, profile customization, friend search, and instant messaging. The application was built using React.js with Context API for state management and React Router for navigation, while Firebase Authentication and Firestore were used for secure login and real-time data synchronization. The platform delivers smooth, real-time communication with a fully responsive design optimized for both mobile and desktop devices.",
+      image: bongochat,
+      tags: ["React JS", "Node.js", "Firebase", "HTML", "CSS", "JavaScript" , "Context API",],
+      github: "https://github.com/Nafis-Foysal-Ashik/chat-app.git",
+      //webapp: "https://www.npmjs.com/package/cmtk-email-validator",
+    },
+    {
+      id: 4,
+      title: "AI Job Finder",
+      description:
+        "Developed a MERN-stack job finder web application featuring an AI-powered search system that matches users with jobs based on skills rather than exact keywords. Implemented semantic query analysis using external AI lookups to interpret user intent (e.g., “frontend”) and map it to related technologies such as HTML, CSS, Tailwind CSS, and Bootstrap before querying the database. The system intelligently suggests relevant job opportunities even without a complete skill match, simplifying the job search process and improving user experience.",
+      image: job,
+      tags: ["JavaScript", "HTML", "CSS" , "React JS", "Node.js", "Express", "MongoDB", "API" , "AI Integration"],
+      github: "https://github.com/Nafis-Foysal-Ashik/AI_Job_Finder.git",
+      //webapp: "chrome://extensions/?id=kngheeibjnnidhfoomkpnbeghackheci",
+    },
+    {
+      id: 5,
+      title: "Blood Bank Management System",
+      description:
+        "Designed and developed an Android application that displays real-time blood availability in nearby hospitals and enables users to directly contact hospitals. The application was built using Android Studio with Firebase Authentication and Firebase Database to handle secure login and CRUD operations, while blood-related information was managed using JSON. The system helps patients find required blood quickly during emergencies and assists hospitals in maintaining accurate and up-to-date blood inventory records.",
+      image: blood,
+      tags: ["Android Studio", "JavaScript", "XML", "Firebase", "REST API" , "JSON",],
+      github: "https://github.com/Nafis-Foysal-Ashik/Blood-Bank-Management-System-Andriod.git",
+      //webapp: "https://webversedigital.com/",
+    },
+    {
+      id: 6,
+      title: "Vehicle Management System",
+      description:
+        "Developed a Vehicle Management System using Oracle SQL to manage vehicle ownership, maintenance schedules, insurance details, and fuel consumption through a structured relational database. Designed a comprehensive database schema with primary and foreign key constraints, table relationships, and complex SQL queries for data insertion, retrieval, updating, and integrity enforcement. The system improves data organization, ensures data integrity, supports cost tracking and regulatory compliance, and enables informed decision-making for efficient vehicle management.",
+      image: vehicle,
+      tags: ["Oracle SQL", "SQL", "Database Design", "ER Diagrams", "Data Integrity", "SQL Queries"],
+      github: "https://github.com/Nafis-Foysal-Ashik/DBMS_Project.git",
+      //webapp: "https://codingmasterweb.in/",
+    },
+    
+  ];  
